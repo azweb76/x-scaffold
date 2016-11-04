ccf54dd86c63c367e268b1015948bc196df652dd
dclayton <dclayton@godaddy.com>
2016-11-04 09:50:05 -0700

    add support for tasks (files, exec, and scaffold)

--- xscaffold/xscaffold.py
+++ xscaffold/xscaffold.py
@@ -35,8 +35,14 @@ readline.parse_and_bind("tab: complete")
 readline.set_completer(complete)
 
 
+class AttributeDict(dict):
+    __getattr__ = dict.__getitem__
+    __setattr__ = dict.__setitem__
+
+
 class color:
     PURPLE = '\033[35m'
+    CYAN = '\033[36m'
     BLUE = '\033[34m'
     GREEN = '\033[32m'
     YELLOW = '\033[33m'
@@ -46,6 +52,19 @@ class color:
     ITALIC = '\033[3m'
     END = '\033[0m'
 
+
+def str2bool(v):
+    if v is None:
+        return False
+    return v.lower() in ("yes", "true", "t", "1", "y")
+
+
+known_types = {
+    'int': int,
+    'bool': str2bool,
+    'str': str,
+    'float': float
+}
 
 def term_color(text, *text_colors):
     return ''.join(text_colors) + text + color.END
@@ -91,6 +110,10 @@ def yaml_format(value):
         return 'null'
     return yaml.dump(value, default_flow_style=True)
 
+def json_format(value):
+    if value is None:
+        return 'null'
+    return json.dumps(value)
 
 def get_parser(path):
     ext = os.path.splitext(path)[1]
@@ -108,6 +131,7 @@ def render(template_name, context, template_dir):
     env = Environment(loader=FileSystemLoader(template_dir))
     env.filters['formatlist'] = format_list
     env.filters['yaml'] = yaml_format
+    env.filters['json'] = json_format
     utils = RenderUtils()
 
     template = env.get_template(template_name)
@@ -195,7 +219,9 @@ class ScaffoldLoader(yaml.Loader):
 
             if d == '' or d is None:
                 if not required:
-                    return convert(item.get('default', d), item.get('type', 'str'))
+                    return item.get('default', None)
+                else:
+                    sys.stdout.write(term_color('[required] ', color.RED))
             else:
                 return convert(d, item.get('type', 'str'))
 
@@ -204,14 +230,9 @@ ScaffoldLoader.add_constructor('!prompt', ScaffoldLoader.prompt)
 
 
 def convert(v, type):
-    if type == 'bool':
-        return str2bool(v)
+    if type in known_types:
+        return known_types[type](v)
     return str(v)
-
-def str2bool(v):
-    if v is None:
-        return False
-    return v.lower() in ("yes", "true", "t", "1", "y")
 
 
 def main():
@@ -223,14 +244,25 @@ def main():
                 options = yaml.load(fhd)
 
         parser = argparse.ArgumentParser(description='Scaffold a directory of files.')
+
+        parser.add_argument(
+            '-l',
+            '--log-level',
+            dest='log_level',
+            default='INFO',
+            choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'],
+            help='optional. Set the log level.')
+
         parser.add_argument('-u', '--url', default=options.get('url', 'https://github.com'), help='github repo url')
         parser.add_argument('-t', '--temp', default=options.get('temp_dir', tempfile.gettempdir()), help='temporary directory')
 
         subparsers = parser.add_subparsers(help='actions')
 
         parser_a = subparsers.add_parser('apply', help='scaffold a directory')
-        parser_a.add_argument('name', help='package name')
+        parser_a.add_argument('package', help='package name')
         parser_a.add_argument('-v', '--version', default='master', help='package version')
+        parser_a.add_argument('-n', '--name', default='scaffold', help='name of scaffold file (<name>.yaml)')
+        parser_a.add_argument('-x', '--extend-context', default=True, help='allow child packages to extend context')
         parser_a.set_defaults(func=apply_cli)
 
         parser_a = subparsers.add_parser('config', help='configure this process')
@@ -238,6 +270,8 @@ def main():
         parser_a.set_defaults(func=config_cli)
 
         args = parser.parse_args()
+        logging.basicConfig(level=getattr(logging, args.log_level))
+
         args.func(args)
     except KeyboardInterrupt:
         exit(0)
@@ -258,35 +292,100 @@ def config_cli(args):
     elif args.action == 'view':
         sys.stdout.write('url: %s' % options.get('url', 'not defined'))
 
+
+def execute_command(context, pkg_dir, commands):
+    for c in commands:
+        cmd = c.format(pkg_dir=pkg_dir, context=AttributeDict(context))
+        rc = os.system(cmd)
+        if rc != 0:
+            raise RuntimeError('Failed to execute command')
+
+
 def apply_cli(args):
-    dest = os.path.join(args.temp, args.name)
+    execute_scaffold({}, args)
 
-    if os.path.exists(dest):
-        rc = os.system("""(cd {dest} && git pull >/dev/null 2>&1)""".format(dest=dest))
+
+def execute_scaffold(parent_context, args):
+    if os.path.exists(args.package):
+        pkg_dir = args.package
     else:
-        rc = os.system("""
-    git clone {url}/{name} {dest} >/dev/null 2>&1
-    """.format(dest=dest, url=args.url, name=args.name))
-    if rc != 0:
-        sys.stdout.write('Failed to pull scaffold package %s' % args.name)
+        pkg_dir = os.path.join(args.temp, args.package)
 
-    rc = os.system("""(cd {dest} && git checkout -f {version} >/dev/null 2>&1)""".format(version=args.version, dest=dest))
-    if rc != 0:
-        sys.stdout.write('Failed to load version %s' % args.version)
+        if os.path.exists(pkg_dir):
+            rc = os.system("""(cd {pkg_dir} && git pull >/dev/null 2>&1)""".format(pkg_dir=pkg_dir))
+        else:
+            rc = os.system("""
+        git clone {url}/{package} {pkg_dir} >/dev/null 2>&1
+        """.format(pkg_dir=pkg_dir, url=args.url, package=args.package))
+        if rc != 0:
+            sys.stdout.write('Failed to pull scaffold package %s' % args.package)
 
-    scaffold_file = os.path.join(dest, './scaffold.yaml')
+        rc = os.system("""(cd {pkg_dir} && git checkout -f {version} >/dev/null 2>&1)""".format(version=args.version, pkg_dir=pkg_dir))
+        if rc != 0:
+            sys.stdout.write('Failed to load version %s' % args.version)
+
+    scaffold_file = os.path.join(pkg_dir, '%s.yaml' % args.name)
     if os.path.exists(scaffold_file):
         with open(scaffold_file, 'r') as fhd:
             config = yaml.load(fhd, ScaffoldLoader)
     else:
+        log.warn('scaffold file %s not found', scaffold_file)
         config = {}
 
-    for f in config.get('files', []):
-        paths = glob2.glob(os.path.join(dest, f['name']))
-        for p in paths:
-            content = render_file(p, config.get('context', {}))
-            with open(f['target'], 'w') as fhd:
+    if args.extend_context:
+        context = dict(parent_context, **config.get('context', {}))
+    else:
+        context = config.get('context', {})
+        context['parent'] = parent_context
+
+    files = config.get('files', [])
+
+    render_files(context, pkg_dir, files)
+
+    tasks = config.get('tasks', [])
+    for task in tasks:
+        sys.stdout.write(term_color('[task] %s' % task['task'], color.CYAN) + '\n')
+        if 'files' in task:
+            render_files(context, pkg_dir, task['files'])
+        if 'exec' in task:
+            execute_command(context, pkg_dir, task['exec'])
+        if 'scaffold' in task:
+            scaffold = AttributeDict(dict({
+                'url': args.url,
+                'temp': args.temp,
+                'version': args.version,
+                'extend_context': args.extend_context
+            }, **task['scaffold']))
+
+            execute_scaffold(context, scaffold)
+
+
+def render_files(context, pkg_dir, files):
+    for f in files:
+        target = f['target']
+        if not target.endswith('/'):
+            p = os.path.join(pkg_dir, f['name'])
+            base, name = os.path.split(p)
+            tbase, tname = os.path.split(target)
+            if not os.path.exists(tbase):
+                os.makedirs(tbase)
+            content = render_file(p, context)
+            with open(target, 'w') as fhd:
                 fhd.write(content)
+        else:
+            source = os.path.join(pkg_dir, f['name'])
+            sbase, sname = os.path.split(source)
+            paths = glob2.glob(source)
+
+            for p in paths:
+                tfile = p[len(sbase) + 1:]
+                t = os.path.join(target, tfile)
+                tbase, tname = os.path.split(t)
+                if not os.path.exists(tbase):
+                    os.makedirs(tbase)
+                content = render_file(p, context)
+                with open(t, 'w') as fhd:
+                    fhd.write(content)
 
 
 if __name__ == '__main__':

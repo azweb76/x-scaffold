d806cd0a857a49ec290959349274cf79dea02bf5
dclayton <dclayton@godaddy.com>
2017-02-22 11:22:45 -0700

    Allow readline to fail on windows

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="xscaffold",
-    version="1.0.9",
+    version="1.0.10",
     install_requires=[
         'PyYaml',
         'jinja2',
--- xscaffold/xscaffold.py
+++ xscaffold/xscaffold.py
@@ -3,7 +3,6 @@
 
 import argparse
 import os
-import readline
 import yaml
 import glob2
 import logging
@@ -28,15 +27,20 @@ def complete(text, state):
         p = text
         home = None
 
-    items = glob2.glob(p+'*')
+    items = glob2.glob(p + '*')
     if items is not None and home is not None:
         items = ['~/' + x[len(home):] for x in items]
-    return (items+[None])[state]
+    return (items + [None])[state]
 
 
-readline.set_completer_delims(' \t\n;')
-readline.parse_and_bind("tab: complete")
-readline.set_completer(complete)
+def set_readline():
+    try:
+        import readline
+        readline.set_completer_delims(' \t\n;')
+        readline.parse_and_bind("tab: complete")
+        readline.set_completer(complete)
+    except:
+        pass
 
 
 class AttributeDict(dict):
@@ -176,11 +180,101 @@ def is_enabled(options):
     return True
 
 
+class Prompt:
+
+    def __init__(self, d):
+        self._dict = d
+        self._value = None
+
+    def get(self):
+        if self._value is None:
+            self._value = self._get_value(self._dict)
+        return self._value
+
+    def _get_value(self, prompt):
+        default = prompt.get('default', None)
+        if isinstance(default, str):
+            default = default.format(env=os.environ)
+
+        if not is_enabled(prompt):
+            return default
+
+        required = prompt.get('required', False)
+        while True:
+            s = term_color('%s: ' % prompt['text'].format(
+                default=default, env=os.environ), color.BOLD)
+            if 'description' in prompt:
+                desc = term_color('%s' % prompt['description'], color.ITALIC)
+                sys.stdout.write('%s\n' % desc)
+
+            if 'choices' in prompt:
+                s = term_color('%s: ' % prompt['text'].format(
+                    default=default), color.BOLD)
+                sys.stdout.write('%s\n\n' % s)
+
+                choices = prompt['choices']
+                while True:
+                    opts = []
+                    max_len = 0
+                    for c in choices:
+                        keywords = ', '.join(c['keywords'])
+                        if len(keywords) > max_len:
+                            max_len = len(keywords)
+                        opts.append({'kw': keywords, 't': c['text']})
+
+                    for opt in opts:
+                        s = '%s' % c['text']
+                        opt['kw'] = opt['kw'].ljust(max_len)
+                        sys.stdout.write('[{kw}] {t}\n'.format(**opt))
+
+                    d = raw_input(term_color('\nchoice: ', color.BOLD))
+
+                    for c in choices:
+                        if d in c['keywords']:
+                            v = c.get('value', d)
+                            if isinstance(v, dict):
+                                v = defaultdict(
+                                    lambda: '', d.get('default', {}), **v)
+                            return v
+
+                    sys.stdout.write('\n%s please select a keyword on the left\n\n' %
+                                     term_color('[invalid choice] ', color.RED))
+            else:
+                if prompt.get('secure', False):
+                    d = getpass.getpass(prompt=s)
+                else:
+                    d = raw_input(s)
+
+            if d == '' or d is None:
+                if not required:
+                    return default
+                else:
+                    sys.stdout.write(term_color('[required] ', color.RED))
+            else:
+                if 'validate' in prompt:
+                    matches = re.match(prompt['validate'], d)
+                    if matches is None:
+                        sys.stdout.write(term_color(
+                            '[invalid, %s] ' % prompt['validate'], color.RED))
+                        continue
+                if 'load' in prompt:
+                    if prompt['load'] == 'yaml':
+                        with open(d, 'r') as fhd:
+                            return yaml.load(fhd)
+                return convert(d, prompt.get('type', 'str'))
+
+
 class ScaffoldLoader(yaml.Loader):
+
     def __init__(self, stream):
         if stream is not None:
             if isinstance(stream, file):
                 self._root = os.path.split(stream.name)[0]
+            elif isinstance(stream, dict):
+                d = stream
+                stream = d['fhd']
+                self._root = os.path.splitext(stream.name)[0]
+                self._context = d['context']
 
         super(ScaffoldLoader, self).__init__(stream)
 
@@ -199,70 +293,15 @@ class ScaffoldLoader(yaml.Loader):
     def prompt(self, node):
         item = self.construct_mapping(node, 9999)
 
-        default = item.get('default', None)
-        if isinstance(default, str):
-            default = default.format(env=os.environ)
+        return Prompt(item).get()
 
-        if not is_enabled(item):
-            return default
+    def prompt2(self, node):
+        item = self.construct_mapping(node, 9999)
 
-        required = item.get('required', False)
-        while True:
-            s = term_color('%s: ' % item['text'].format(default=default, env=os.environ), color.BOLD)
-            if 'description' in item:
-                desc = term_color('%s' % item['description'], color.ITALIC)
-                sys.stdout.write('%s\n' % desc)
-
-            if 'choices' in item:
-                s = term_color('%s: ' % item['text'].format(default=default), color.BOLD)
-                sys.stdout.write('%s\n\n' % s)
-
-                choices = item['choices']
-                while True:
-                    opts = []
-                    max_len = 0
-                    for c in choices:
-                        keywords = ', '.join(c['keywords'])
-                        if len(keywords) > max_len:
-                            max_len = len(keywords)
-                        opts.append({ 'kw': keywords, 't': c['text']})
-
-                    for opt in opts:
-                        s = '%s' % c['text']
-                        opt['kw'] = opt['kw'].ljust(max_len)
-                        sys.stdout.write('[{kw}] {t}\n'.format(**opt))
-
-                    d = raw_input(term_color('\nchoice: ', color.BOLD))
-
-                    for c in choices:
-                        if d in c['keywords']:
-                            v = c.get('value', d)
-                            if isinstance(v, dict):
-                                v = defaultdict(lambda: '', item.get('default', {}), **v)
-                            return v
-
-                    sys.stdout.write('\n%s please select a keyword on the left\n\n' %
-                                     term_color('[invalid choice] ', color.RED))
-            else:
-                if item.get('secure', False):
-                    d = getpass.getpass(prompt=s)
-                else:
-                    d = raw_input(s)
-
-            if d == '' or d is None:
-                if not required:
-                    return default
-                else:
-                    sys.stdout.write(term_color('[required] ', color.RED))
-            else:
-                if 'validate' in item:
-                    matches = re.match(item['validate'], d)
-                    if matches is None:
-                        sys.stdout.write(term_color('[invalid, %s] ' % item['validate'], color.RED))
-                        continue
-                return convert(d, item.get('type', 'str'))
+        return Prompt(item)
 
 
+ScaffoldLoader.add_constructor('!prompt2', ScaffoldLoader.prompt2)
 ScaffoldLoader.add_constructor('!prompt', ScaffoldLoader.prompt)
 ScaffoldLoader.add_constructor('!module', ScaffoldLoader.module)
 
@@ -276,12 +315,14 @@ def convert(v, type):
 def main():
     try:
         options = {}
+        set_readline()
         scaffold_file = os.path.expanduser('~/.xscaffold')
         if os.path.exists(scaffold_file):
             with open(scaffold_file, 'r') as fhd:
                 options = yaml.load(fhd)
 
-        parser = argparse.ArgumentParser(description='Scaffold a directory of files.')
+        parser = argparse.ArgumentParser(
+            description='Scaffold a directory of files.')
 
         parser.add_argument(
             '-l',
@@ -292,22 +333,28 @@ def main():
             help='optional. Set the log level.')
 
         parser.add_argument('-u', '--url', default=options.get('url', 'https://github.com'), help='github repo url')
-        parser.add_argument('-t', '--temp', default=options.get('temp_dir', tempfile.gettempdir()), help='temporary directory')
+        parser.add_argument('-t', '--temp', default=options.get('temp_dir',tempfile.gettempdir()), help='temporary directory')
 
         subparsers = parser.add_subparsers(help='actions')
 
         parser_a = subparsers.add_parser('apply', help='scaffold a directory')
         parser_a.add_argument('package', help='package name')
-        parser_a.add_argument('-v', '--version', default='master', help='package version')
-        parser_a.add_argument('-n', '--name', default='scaffold', help='name of scaffold file (<name>.yaml)')
-        parser_a.add_argument('-x', '--extend-context', default=True, help='allow child packages to extend context')
+        parser_a.add_argument(
+            '-v', '--version', default='master', help='package version')
+        parser_a.add_argument('-n', '--name', default='scaffold',
+                              help='name of scaffold file (<name>.yaml)')
+        parser_a.add_argument('-x', '--extend-context', default=None,
+                              help='allow child packages to extend context')
         parser_a.set_defaults(func=apply_cli)
 
-        parser_a = subparsers.add_parser('config', help='configure this process')
-        parser_a.add_argument('action', default='view', help='save or view configuration')
+        parser_a = subparsers.add_parser(
+            'config', help='configure this process')
+        parser_a.add_argument('action', default='view',
+                              help='save or view configuration')
         parser_a.set_defaults(func=config_cli)
 
-        parser_a = subparsers.add_parser('upgrade', help='upgrade x-scaffold using pip')
+        parser_a = subparsers.add_parser(
+            'upgrade', help='upgrade x-scaffold using pip')
         parser_a.set_defaults(func=upgrade_cli)
 
         args = parser.parse_args()
@@ -320,7 +367,8 @@ def main():
 
 
 def upgrade_cli(args):
-    rc = os.system('pip install git+https://github.com/azweb76/x-scaffold --upgrade >/dev/null 2>&1')
+    rc = os.system(
+        'pip install git+https://github.com/azweb76/x-scaffold --upgrade >/dev/null 2>&1')
     if rc == 0:
         log('x-scaffold was {GREEN}successfully{END} upgraded.')
     else:
@@ -402,6 +450,14 @@ def rm_rf(d):
     os.rmdir(d)
 
 
+def process_prompts(d):
+    for x in d:
+        if isinstance(d[x], Prompt):
+            d[x] = d[x].get()
+        elif isinstance(d[x], dict):
+            process_prompts(d[x])
+
+
 def execute_scaffold(parent_context, args, todos, notes):
     if os.path.exists(args.package):
         sys.stdout.write(
@@ -413,7 +469,8 @@ def execute_scaffold(parent_context, args, todos, notes):
         rc = 9999
         if os.path.exists(pkg_dir):
             log('{YELLOW}[git] updating %s package...{END}\n' % args.package)
-            rc = os.system("""(cd {pkg_dir} && git pull >/dev/null 2>&1)""".format(pkg_dir=pkg_dir))
+            rc = os.system(
+                """(cd {pkg_dir} && git pull >/dev/null 2>&1)""".format(pkg_dir=pkg_dir))
             if rc != 0:
                 log('{RED}[error]{YELLOW} package %s is having issues, repairing...{END}\n' % args.package)
                 rm_rf(pkg_dir)
@@ -425,9 +482,11 @@ def execute_scaffold(parent_context, args, todos, notes):
         git clone {url}/{package} {pkg_dir} >/dev/null 2>&1
         """.format(pkg_dir=pkg_dir, url=args.url, package=args.package))
         if rc != 0:
-            sys.stdout.write('Failed to pull scaffold package %s' % args.package)
+            sys.stdout.write(
+                'Failed to pull scaffold package %s' % args.package)
 
-        rc = os.system("""(cd {pkg_dir} && git checkout -f {version} >/dev/null 2>&1)""".format(version=args.version, pkg_dir=pkg_dir))
+        rc = os.system("""(cd {pkg_dir} && git checkout -f {version} >/dev/null 2>&1)""".format(
+            version=args.version, pkg_dir=pkg_dir))
         if rc != 0:
             sys.stdout.write('Failed to load version %s' % args.version)
 
@@ -435,17 +494,22 @@ def execute_scaffold(parent_context, args, todos, notes):
     scaffold_file = os.path.join(pkg_dir, '%s.yaml' % args.name)
     if os.path.exists(scaffold_file):
         with open(scaffold_file, 'r') as fhd:
-            config = yaml.load(fhd, ScaffoldLoader)
+            config = yaml.load({
+                'fhd': fhd,
+                'context': parent_context
+            }, ScaffoldLoader)
     else:
         _log.warning('scaffold file %s not found', scaffold_file)
         config = {}
 
     if args.extend_context:
-        context = defaultdict(lambda: '', parent_context, **config.get('context', {}))
+        context = defaultdict(lambda: '', parent_context,
+                              **args.extend_context)
     else:
         context = config.get('context', {})
         context['parent'] = parent_context
 
+    process_prompts(context)
     files = config.get('files', [])
 
     context['pkg_dir'] = pkg_dir
@@ -455,7 +519,8 @@ def execute_scaffold(parent_context, args, todos, notes):
     for task in tasks:
         if is_enabled(task):
             if 'task' in task:
-                sys.stdout.write(term_color('[task] %s' % task['task'], color.CYAN) + '\n')
+                sys.stdout.write(term_color('[task] %s' % task[
+                                 'task'], color.CYAN) + '\n')
             if 'files' in task:
                 render_files(context, pkg_dir, task['files'])
             if 'exec' in task:
@@ -473,14 +538,16 @@ def execute_scaffold(parent_context, args, todos, notes):
 
                 context = execute_scaffold(context, scaffold, todos, notes)
             if 'log' in task:
-                sys.stdout.write(term_color('%s' % task['log'].format(**context), color.YELLOW) + '\n')
+                sys.stdout.write(term_color(
+                    '%s' % task['log'].format(**context), color.YELLOW) + '\n')
             if 'todo' in task:
                 todos.append(task['todo'])
 
     if 'notes' in config:
         notes.append(config['notes'])
 
-    sys.stdout.write(term_color('[done] scaffolding %s::%s complete!' % (args.package, args.name), color.CYAN) + '\n')
+    sys.stdout.write(term_color('[done] scaffolding %s::%s complete!' % (
+        args.package, args.name), color.CYAN) + '\n')
 
     return context
 
